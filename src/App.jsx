2361557cb5e2bd71795cd99c5eed505cb856bb4e
tratchapong <tratchapong@gmail.com>
2025-03-12 11:55:20 +0700

    start tw4+daisy5

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <div className="flex flex-col gap-3 max-w-5xl ">
+      <div className="text-pink-500 text-3xl bg-lime-300 border">Codecamp19</div>
+      <button className="btn btn-primary">OK</button>
+    </div>
+  )
+}
+
+export default App
